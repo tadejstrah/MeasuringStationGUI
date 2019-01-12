e21373023f2927e118b4f11675d9c0f2c1c61f45
tadejstrah <tadej.strah@gmail.com>
2019-01-12 22:59:36 +0100

    basic layout; a bunch of examples...

---
+++ main.py
@@ -0,0 +1,72 @@
+
+from tkinter import *
+from tkinter.ttk import * 
+from tkinter import ttk
+
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg#, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+
+
+root = Tk()
+root.title("Serial to graph")
+
+s = Style()
+s.configure("TFrame", background="red")
+#s.configure("TLabel",backgorund="blue")
+
+root.columnconfigure(0,weight=1)
+root.rowconfigure(0,weight=1)
+root.configure(background="blue")
+
+mainframe = ttk.Frame(root,style="TFrame", borderwidth=0) #red red red red red
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))    
+mainframe.columnconfigure(0,weight=1)
+mainframe.rowconfigure(0,weight=1)
+
+
+s2 = Style()
+s2.configure("neki.TFrame",background="yellow")
+testFrame = ttk.Frame(mainframe,style="neki.TFrame",padding="100 100 100 100")
+testFrame.grid(column=1,row=0,sticky=(N,S))
+
+#testFrame.pack(side=LEFT,expand=YES,fill=BOTH)
+
+s3 = Style()
+s3.configure("bhus.TFrame",background="green")
+testFrame2=ttk.Frame(mainframe,style="bhus.TFrame",borderwidth=10)
+testFrame2.grid(column=0,row=0,sticky=(N,E,S,W))
+testFrame2.rowconfigure(0,weight=1)
+testFrame2.columnconfigure(0,weight=1)
+
+ttk.Label(testFrame2,text="shdikjgndls",background="white",padding="100 100 100 100").grid(column=0,row=0,sticky=(N,S,W,E))
+
+ttk.Label(testFrame,text="nekineki",background="purple").grid(column=0,row=0)
+
+
+f = Figure(figsize=(5,5), dpi=100)
+a = f.add_subplot(111)
+a.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
+
+canvas = FigureCanvasTkAgg(f,master=testFrame2)
+canvas.get_tk_widget().grid(column=0,row=0,sticky=(N,S,W,E))
+# canvas.show()
+#canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
+
+#toolbar = NavigationToolbar2TkAgg(canvas, self)
+# toolbar.update()
+#canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=True)
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
---
+++ test3.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+from tkinter import N,S,E,W
+
+
+LARGE_FONT= ("Verdana", 12)
+
+class SeaofBTCapp(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
+        #container.pack(side="top", fill="both", expand = True)
+        container.grid(column=0,row=0,sticky=(N,E,S,W))
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        container.configure(background="red",borderwidth=10)
+        
+
+        self.frames = {}
+
+        frame = StartPage(container, self)
+
+        self.frames[StartPage] = frame
+
+        frame.grid(row=0, column=0, sticky=(N,E,S,W))
+
+        self.show_frame(StartPage)
+    
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+        
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label.grid(column=0,row=0,sticky=(E))
+       # label.pack(pady=10,padx=10)
+
+
+app = SeaofBTCapp()
+app.columnconfigure(0,weight=1)
+app.rowconfigure(0,weight=1)
+app.mainloop()
