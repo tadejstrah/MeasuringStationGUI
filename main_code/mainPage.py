f74b65e3c22aea76d52e6b9aebe4b6b0579c3df5
tadejstrah <tadej.strah@gmail.com>
2019-07-21 22:24:07 +0200

    refresh rate back to 10Hz

--- main_code/mainPage.py
+++ main_code/mainPage.py
@@ -69,7 +69,7 @@ class mainPage(ttk.Frame):
         toolbar.update()
 
         
-        self.ani = FuncAnimation(fig, self. run, interval=1000,repeat=True)
+        self.ani = FuncAnimation(fig, self. run, interval=100,repeat=True)
 
         commandsFrame = ttk.Frame(self)
         commandsFrame.grid(column=1,row=0,sticky=(E,W,N))
